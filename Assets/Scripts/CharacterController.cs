28ec5743e0d4de75bab1603f3027a725e1077827
salihkarakus <sskarakussalih77@gmail.com>
2025-01-20 20:53:29 +0300

    character controller script is done

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,59 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterContoller : MonoBehaviour
 {
+    [SerializeField] private float speed = 4f;
+    [SerializeField] private float jumpForce = 300f;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
 
-[SerializeField] private float speed = 5f;
-[SerializeField] private float jumpForce = 400f;
+    private bool grounded;
+    private bool started;
+    private bool jumping;
 
-private Rigidbody2D _rigidbody2D;
-private Animator _animator;
-
-private bool grounded;
-private bool started;
-private bool jumping;
-private void Awake()
-{
-
-_rigidbody2D = GetComponent<Rigidbody2D>(); //caching
-_animator = GetComponent<Animator>(); //caching
-grounded = true;
-
-}
-
-private void Update()
-{
-
-if(Input.GetKeyDown("space"))
-{
-    
-}
-
-}
-
-private void FixedUpdate()
-{
-    if(started)
+    private void Awake()
     {
-    _rigidbody2D.velocity = new Vector2(speed, _rigidbody2D.velocity.y);
-    }
-
-    if(jumping)
-    {
-        _rigidbody2D.AddForce(new Vector2(0f,jumpForce));
-        jumping = false;
-    }
-}
-
-private void OnCollisionEnter2D(Collision2D other)
-{
-    if(other.gameObject.CompareTag("Ground"))
-    {
+        _rigidbody2D = GetComponent<Rigidbody2D>(); //caching
+        _animator = GetComponent<Animator>();
         grounded = true;
     }
-}
 
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+
+            if (started && grounded)
+            {
+                _animator.SetTrigger("Jump");
+                grounded = false;
+                jumping = true;
+            }
+            else
+            {
+                _animator.SetBool("GameStarted", true);
+                started = true;
+            }
+        }
+
+        
+            _animator.SetBool("Grounded", grounded);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (started) 
+        {
+            _rigidbody2D.velocity = new Vector2(speed , _rigidbody2D.velocity.y);
+        }
+
+        if (jumping)
+        {
+            _rigidbody2D.AddForce(new Vector2(0f , jumpForce)); 
+            jumping = false;    
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+        }
+    }
 }
+ 
