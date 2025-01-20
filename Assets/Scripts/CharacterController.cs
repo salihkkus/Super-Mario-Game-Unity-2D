6e3fa976f35c7004bb0f2dd51eb06460eb62add4
salihkarakus <sskarakussalih77@gmail.com>
2025-01-20 20:08:22 +0300

    fixed update and new variables added

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,35 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
 
 [SerializeField] private float speed = 10f;
-[SerializeField] private float jumpForge = 5f;
+[SerializeField] private float jumpForce = 5f;
 
 private Rigidbody2D _rigidbody2D;
 private Animator _animator;
 
 private bool grounded;
 private bool started;
+private bool jumping;
 private void Awake()
 {
 
 _rigidbody2D = GetComponent<Rigidbody2D>(); //caching
 _animator = GetComponent<Animator>(); //caching
-started = true;
+grounded = true;
 
 }
 
 private void Update()
 {
 
-if(Input.GetKey("space"))
+if(Input.GetKeyDown("space"))
 {
-    Debug.Log("Jumping");
+    
 }
 
 }
+
+private void FixedUpdate()
+{
+    if(started)
+    {
+    _rigidbody2D.velocity = new Vector2(speed, _rigidbody2D.velocity.y);
+    }
+
+    if(jumping)
+    {
+        _rigidbody2D.AddForce(new Vector2(0f,jumpForce));
+    }
+}
+
 
 }
