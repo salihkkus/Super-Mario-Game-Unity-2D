d4caed12abd9ccab7bf2e2aad89190927338cc32
salihkarakus <sskarakussalih77@gmail.com>
2025-01-20 17:11:46 +0300

    variables awake and update created

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[SerializeField] private float speed = 10f;
+[SerializeField] private float jumpForge = 5f;
+
+private Rigidbody2D _rigidbody2D;
+private Animator _animator;
+
+private bool grounded;
+private bool started;
+private void Awake()
+{
+
+_rigidbody2D = GetComponent<Rigidbody2D>(); //caching
+_animator = GetComponent<Animator>(); //caching
+started = true;
+
+}
+
+private void Update()
+{
+
+if(Input.GetKey("space"))
+{
+    Debug.Log("Jumping");
+}
+
+}
+
 }
