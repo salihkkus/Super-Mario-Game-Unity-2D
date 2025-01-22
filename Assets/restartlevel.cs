e8564a09cc64c626bf86f605d384fb9cc4727043
salihkarakus <sskarakussalih77@gmail.com>
2025-01-22 18:37:25 +0300

    restart level and next level scene script is done

---
+++ Assets/restartlevel.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartlevel : MonoBehaviour
+{
+
+private void OnTriggerEnter2D(Collider2D other)
+{
+    if(other.CompareTag("Player"))
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
+}
